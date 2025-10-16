384b0fa933efbb85c1d10ae4e37206c6abd0e9cd
Aghil Shoja <shoja.aghil@yahoo.com>
2025-10-16 15:08:40 +0330

    feat(auth): add login/signup toggle and remove redundant file.
    - implemented toggle functionality to switch between login and signup pages.
    - removed redundant file that was mistakenly kept in the project and served no purpose.

--- auth/app.js
+++ auth/app.js
@@ -1,9 +1,29 @@
 import { validateInput } from "./username-login-error.js";
 import { validatePasswordInput } from "./password-login-error.js";
+import { toggleSignUp } from "./toggle-signup-page.js";
+import { toggleLogin } from "./toggle-login-page.js";
 
 document.querySelector(".login__form").addEventListener("submit", (e) => {
   e.preventDefault();
   if (validateInput() && validatePasswordInput()) {
-    window.location.href = "index.html";
+    window.location.href = "html/index.html";
+  }
+});
+
+document.body.addEventListener("click", (e) => {
+  const signupButton = document.querySelector(".submit-validation__signup");
+  if (!signupButton) return;
+  const loginButton = document.querySelector(".submit-validation__login");
+  if (!loginButton) return;
+  if (e.target.classList.contains("submit-validation__signup")) {
+    loginButton.classList.remove("submit-validation__login--background");
+    e.target.classList.add("submit-validation__signup--background");
+    toggleSignUp();
+  }
+  if (e.target.classList.contains("submit-validation__login")) {
+    signupButton.classList.remove("submit-validation__signup--background");
+    e.target.classList.add("submit-validation__login--background");
+
+    toggleLogin();
   }
 });
